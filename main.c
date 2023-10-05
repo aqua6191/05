2444b62af8b5c5be80a14abb3dd291d4fd6c1e03
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-10-05 16:47:41 +0900

    06

--- main.c
+++ main.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int sum = 0;
+    int answer = 59;
     int num;
-    int i;
+    int trial = 0; //몇번 숫자를 세는지 
     
-    printf("input a number:");
-    scanf("%d", &num);
-    
-    for (i=0;i<=num;i++){
-        sum = sum+i;
+    do {
+        printf("Guess a number:");
+        scanf("%d", &num);
+        
+        if (num == answer)
+           printf("congratulation! ");
+        else if(num<answer)
+             printf("high!\n");
+        else if(num>answer)
+             printf("low!\n");
+             
+        trial = trial+1; 
         }
     
-    printf("The result is %i\n", sum);
+    while(num != answer);
+    
+    printf("trials: %d\n", trial);
     system("PAUSE");   
     return 0;
 }
